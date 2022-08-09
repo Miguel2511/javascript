784c8dd78df60f000af0a00fda5a1e328606d8c5
Miguel2511 <miguelbernardes2010@outlook.com>
2022-08-09 12:45:27 -0300

    00

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade é ${vel}km/h por hora`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida da via. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
