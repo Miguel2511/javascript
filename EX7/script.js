fa03359b1f9c9e315f5ffc7a855546e43138b775
Miguel2511 <miguelbernardes2010@outlook.com>
2022-08-18 10:43:09 -0300

    end

--- EX7/script.js
+++ EX7/script.js
@@ -1,14 +1,14 @@
 
 var caseNum = []    //vetor para os números
 
-function carregar() {
+function adicionar() {
     numero = document.getElementById('num') //numero inserido
     tab = document.getElementById('area-tab')   //onde os números serão escritos
     divInfo = document.getElementById('divInfo')    //div que contem a area-tab
     item = document.createElement('option') //cria elemento opção
     num = Number(numero.value)  //number da variável
 
-    if(numero.value.length == 0 ){  //testando se tem algo no campo
+    if (numero.value.length == 0 ){  //testando se tem algo no campo
         window.alert('Por favor digite um número')
         divInfo.style.display = 'none'
         console.log('IF')
@@ -36,22 +36,26 @@ function carregar() {
 }
 
 function finalizar() {
-    var info = document.getElementById('info')
-    var nCadastrados = caseNum.length
-    info.innerHTML = (`Ao todo, temos ${nCadastrados} números cadastrados.<br>`)
-    var maiorNum = caseNum.sort(function(a, b){return a-b})
-    
-    var soma = 0
-    for (var c = 0; c < caseNum.length; c++) {  //soma os valores
-        soma += caseNum[c]
-    }
+    if (caseNum == '') {
+        window.alert('Por favor digite um número')
+    }else {
+        var info = document.getElementById('info')
+        var nCadastrados = caseNum.length
+        info.innerHTML = (`Ao todo, temos ${nCadastrados} números cadastrados.<br>`)
+        var maiorNum = caseNum.sort(function(a, b){return a-b})
+        
+        var soma = 0
+        for (var c = 0; c < caseNum.length; c++) {  //soma os valores
+            soma += caseNum[c]
+        }
 
-    var media = (soma / caseNum.length)
-    console.log('media')
-    info.innerHTML += (`O maior valor informado foi ${maiorNum[nCadastrados - 1]}.<br>`)
-    info.innerHTML += (`O menor valor informado foi ${maiorNum[0]}.<br>`)
-    info.innerHTML += (`Somando todos os valores, temos ${soma}.<br>`)
-    info.innerHTML += (`A média dos valores digitados é:  ${media}<br>`)
+        var media = (soma / caseNum.length)
+        console.log('media')
+        info.innerHTML += (`O maior valor informado foi ${maiorNum[nCadastrados - 1]}.<br>`)
+        info.innerHTML += (`O menor valor informado foi ${maiorNum[0]}.<br>`)
+        info.innerHTML += (`Somando todos os valores, temos ${soma}.<br>`)
+        info.innerHTML += (`A média dos valores digitados é:  ${media}<br>`)
+    }
 }
 
 function limpar() {
