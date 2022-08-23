42e23008a2e9f96e0f2226a8b3731c11a884ecf5
Miguel2511 <miguelbernardes2010@outlook.com>
2022-08-23 13:01:35 -0300

    ss

--- projeto-to-do-2/script.js
+++ projeto-to-do-2/script.js
@@ -1,9 +1,9 @@
 //função que adiciona tarefa
 function addTask() {
 
-    // titulo da tarefa
-    const taskTitle = document.querySelector("#task-title")
-    var task = taskTitle.value
+    // titulo da tarefa (INPUT)
+    const taskTitle = document.querySelector("#input-task-title")
+    var task = taskTitle.value  //valor do input(o que está dentro, escrito no input)
     const list = document.querySelector("#task-list")
     console.log(taskTitle)
     
@@ -15,6 +15,7 @@ function addTask() {
 
         //adiciona o titulo
         newTask.querySelector(".task-title").textContent = task
+        console.log(task)
         
         //remove as classes desnecessárias
         newTask.classList.remove("template")
@@ -22,21 +23,27 @@ function addTask() {
 
         //adicionar dentro da UL
         list.appendChild(newTask)
-        taskTitle.value = ''
+        taskTitle.value = ''  //limpar a area de texto do input
 
+        //botão de close
         newTask.querySelector(".icon-close").addEventListener("click", function() {
             removeTask(this)
         })
 
+        //botão de check
         newTask.querySelector(".icon-check").addEventListener("click", function() {
             check(this)
         })
     }
 }
 
+function teste() {
+    console.log("teste")
+}
+
 function check(task) {
     const taskToComplete = task.parentNode
-    taskToComplete.classList.toggle("donee")
+    taskToComplete.classList.toggle("done")
 }
 
 function removeTask(task) {
