533f992564c8eb2bc5379af1af4e2d33b306033b
Miguel Vilela <miguelbernardes2010@outlook.com>
2022-08-30 19:58:56 -0300

    split

--- Projeto-calculadora/js/script.js
+++ Projeto-calculadora/js/script.js
@@ -1,22 +1,83 @@
-const previusOperation = document.querySelector('#previus-operation')
-const currentOperation = document.querySelector('#current-operation')
+const previusOperationText = document.querySelector('#previus-operation')
+const currentOperationText = document.querySelector('#current-operation')
 const buttons = document.querySelectorAll('#buttons-container button')
 
 console.log(buttons)
 
-class Calculator {}
+class Calculator {
+    // onde inicia os objetos
+    // ex: obj carro na cor azul. Será iniciado aqui
+    constructor(previusOperationText, currentOperationText) {
+        this.previusOperationText = previusOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ""
+    }
+
+    // metodo que pega o numero digitado e guarda dentro de currentOperation
+    addDigit(digit) {
+        //check current operation
+        if (digit === '.' && this.currentOperationText.innerText.includes(".")){
+            return
+        }
+
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+
+    processOperation(operation) {
+        let operationValue
+        // split(" ")[0] - separa por espaço e pega o indice zero(número) 
+        const previous = +this.previusOperationText.innerText.split(" ")[0]
+        const current = +this.currentOperationText.innerText
+
+        switch(operation) {
+            case "+":
+                operationValue = previous + current
+                this.updateScreen(operationValue, operation, current, previous)
+                break
+            default:
+                return
+        }
+    }
+    
+    // metodo que vai atualizar a tela da calculadora
+    updateScreen(
+        operationValue = null, 
+        operation = null, 
+        previous = null, 
+        current = null
+        ) {
+
+        console.log(operationValue, operation, previous, current)
+
+        if(operationValue === null) {
+            // o texto da calculadore recebe o numero digitado
+            this.currentOperationText.innerText += this.currentOperation
+        }else {
+            if(previous === 0) {
+                operationValue = current
+            }
+
+            this.previusOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = ""
+        }
+        
+    }
+
+
+}
+
+const calc = new Calculator(previusOperationText, currentOperationText)
 
 //EVENTO
 buttons.forEach((btn) => {      //ta chamando cada botão de btn
-    btn.addEventListener("click", (e) => {      //adicionando evento de click (e - objeto do evento)
+    btn.addEventListener("click", (e) => {      //adicionando evento de click 
         const value = e.target.innerText
-
-        if(+value >= 0 || value === '.') {
-            console.log(typeof value)
-            console.log(value)
-        }else {
-            console.log(typeof value)
-            console.log(value)
+        
+        if (+value >= 0 || value === ".") {
+            calc.addDigit(value)
+        } else {
+            calc.processOperation(value)
         }
     })
 })
