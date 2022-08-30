ca94d0313ac984fb853bb5ec7c4248a14a4f8322
Miguel2511 <miguelbernardes2010@outlook.com>
2022-08-30 08:16:43 -0300

    sda

--- Projeto-calculadora/js/script.js
+++ Projeto-calculadora/js/script.js
@@ -2,9 +2,24 @@ const previusOperation = document.querySelector('#previus-operation')
 const currentOperation = document.querySelector('#current-operation')
 const buttons = document.querySelectorAll('#buttons-container button')
 
-// console.log(buttons)
+console.log(buttons)
 
 class Calculator {}
+
+//EVENTO
+buttons.forEach((btn) => {      //ta chamando cada botão de btn
+    btn.addEventListener("click", (e) => {      //adicionando evento de click (e - objeto do evento)
+        const value = e.target.innerText
+
+        if(+value >= 0 || value === '.') {
+            console.log(typeof value)
+            console.log(value)
+        }else {
+            console.log(typeof value)
+            console.log(value)
+        }
+    })
+})
 
 // buttons.forEach((btn) => {
 //     btn.addEventListener("click", (e) => {
@@ -14,12 +29,31 @@ class Calculator {}
 //     })
 // })
 
-function btn(bt) {
-    bt.addEventListener("click")
-    fe()    
-}
 
-function fe(e) {
 
-}
-buttons.forEach = btn(bt)
+
+
+
+
+/*
+REFERÊNCIAS ARRAY BUTTONS
+0 - CE
+1 - C
+2 - DEL
+3 - /
+4 - 7
+5 - 8
+6 -9
+7 - *
+8 - 4
+9 - 5
+10 - 6
+11 - -
+12 - 1
+13 - 2
+14 - 3
+15 - +
+16 - 0
+17 - .
+18 - =
+*/
